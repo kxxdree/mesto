c41f0ac2a310c4d76944cec7e5768cd65231d699
kxxdree <truebackward@gmail.com>
2022-11-23 22:52:44 +0300

    add save feature

--- script/script.js
+++ script/script.js
@@ -4,14 +4,33 @@ let profile = content.querySelector('.profile');
 let editButton = profile.querySelector('.profile__edit_button');
 let popup = container.querySelector('.popup');
 let popupClose = popup.querySelector('.popup__close');
+let formElement = popup.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_name')
+let jobInput = popup.querySelector('.popup__input_subscription');
+let profileTitle = profile.querySelector('.profile__title');
+let profileSubtitle = profile.querySelector('.profile__subtitle');
+let saveButton = formElement.querySelector('.popup__save');
+
 
 function showClick() {
     popup.classList.add('popup__opened');
+    nameInput.textContent = profileTitle.value;
+    jobInput.textContent = profileSubtitle.value;
 }
 
 function closeClick() {
     popup.classList.remove('popup__opened');
 }
 
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    closeClick();
+
+}
+
 editButton.addEventListener('click', showClick);
 popupClose.addEventListener('click', closeClick);
+formElement.addEventListener('submit', formSubmitHandler);
+saveButton.addEventListener('submit', formSubmitHandler);
