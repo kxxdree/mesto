578c7d247ce604d4d09f4e27358bd5a0126fd1c5
kxxdree <truebackward@gmail.com>
2022-11-23 21:15:31 +0300

    add popup close feature

--- script/script.js
+++ script/script.js
@@ -1,20 +1,17 @@
 let container = document.querySelector('.page');
-let profile = container.querySelector('.profile');
+let content = container.querySelector('.content');
+let profile = content.querySelector('.profile');
 let editButton = profile.querySelector('.profile__edit_button');
 let popup = container.querySelector('.popup');
 let popupClose = popup.querySelector('.popup__close');
-let formElement = popup.querySelector('popup__form');
-
 
 function showClick() {
     popup.classList.add('popup__opened');
 }
 
-function closeClick () {
+function closeClick() {
     popup.classList.remove('popup__opened');
 }
 
 editButton.addEventListener('click', showClick);
 popupClose.addEventListener('click', closeClick);
-
-
